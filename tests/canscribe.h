d3d9cf1ad815070318ba4b6acb89ca13bbdbaab4
waseemR02 <waseem.riaz.9999@gmail.com>
2024-04-25 12:48:46 +0530

    Move a copy of canscribe srcs to tests
    Signed-off-by: waseemR02 <waseem.riaz.9999@gmail.com>

---
+++ tests/canscribe.h
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#define CAN_MAX_DLEN    8U
+#define DIV_ROUND_UP(n, d) (((n) + (d) - 1) / (d))
+
+struct can_frame {
+	uint32_t id  : 29;
+	uint8_t res0 : 3; /* reserved/padding. */
+	uint8_t dlc;
+	uint8_t flags;
+	uint16_t res1;  /* reserved/padding. */
+	union {
+		uint8_t data[CAN_MAX_DLEN];
+		uint32_t data_32[DIV_ROUND_UP(CAN_MAX_DLEN, sizeof(uint32_t))];
+	};
+};
+
+struct canscribe_msg {
+	struct can_frame frame;
+	uint32_t crc;
+};
+
+void serialize(uint8_t *buf, uint8_t *msg, int len);
+void deserialize(uint8_t *buf, uint8_t *msg, int len);
